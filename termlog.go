bce8c554cc853647518f66afd2c8ae158b991802
Kendall Zettlmeier <kendall@zettlmeier.com>
2020-09-02 16:41:22 -0500

    GTA-55 - Create a new NewConsoleErrorLogWriter which allows us to allow writing to stderr if the log level is ERROR or CRITICAL

--- termlog.go
+++ termlog.go
@@ -16,27 +16,33 @@ type ConsoleLogWriter chan *LogRecord
 
 // This creates a new ConsoleLogWriter writing to stdout
 func NewConsoleLogWriter() ConsoleLogWriter {
-	return newConsoleLogWriterWithIOWriter(stdout)
-}
-
-// This creates a new ConsoleLogWriter writing to stderr
-func NewConsoleErrorLogWriter() ConsoleLogWriter {
-	return newConsoleLogWriterWithIOWriter(stderr)
-}
-
-func newConsoleLogWriterWithIOWriter(writer io.Writer) ConsoleLogWriter {
 	records := make(ConsoleLogWriter, LogBufferLength)
-	go records.run(writer)
+	go records.runWithoutErrorWriter(stdout)
 	return records
 }
 
-func (w ConsoleLogWriter) run(out io.Writer) {
+// This creates a new ConsoleLogWriter writing to ERROR/CRITICAL TO stderr and everything else to stdout
+func NewConsoleErrorLogWriter() ConsoleLogWriter {
+	records := make(ConsoleLogWriter, LogBufferLength)
+	go records.run(stdout, stderr)
+	return records
+}
+
+func (w ConsoleLogWriter) runWithoutErrorWriter(out io.Writer) {
+	w.run(out, nil)
+}
+
+func (w ConsoleLogWriter) run(normalOut io.Writer, errOut io.Writer) {
 	var timestr string
 	var timestrAt int64
 
 	for rec := range w {
 		if at := rec.Created.UnixNano() / 1e9; at != timestrAt {
 			timestr, timestrAt = rec.Created.Format("01/02/06 15:04:05"), at
+		}
+		out := normalOut
+		if errOut != nil && (rec.Level == ERROR || rec.Level == CRITICAL) {
+			out = errOut
 		}
 		fmt.Fprint(out, "[", timestr, "] [", levelStrings[rec.Level], "] ", rec.Message, "\n")
 	}
